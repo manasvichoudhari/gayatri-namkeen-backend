68895b37f5ab16e3c1ebe56c694d9a3b4e4e3f6b
Manasvi Choudhari <160049511+manasvichoudhari@users.noreply.github.com>
2026-07-22 17:54:56 +0530

    debugging

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,60 +3,41 @@ const User = require("../models/User");
 
 const protect = async (req, res, next) => {
   try {
-
     const authHeader = req.headers.authorization;
 
-
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-
       return res.status(401).json({
         success: false,
-        message: "No token provided"
+        message: "No token provided",
       });
-
     }
 
-
+    console.log("Authorization Header:", req.headers.authorization);
     const token = authHeader.split(" ")[1];
-
-
-    const decoded = jwt.verify(
-      token,
-      process.env.JWT_SECRET
-    );
+    console.log("Received Token:", token);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log("Decoded:", decoded);
-    const user = await User.findById(decoded.id)
-      .select("-password");
-      console.log("User:", user);
-
+    const user = await User.findById(decoded.id).select("-password");
+    console.log("User:", user);
 
     if (!user) {
-
       return res.status(401).json({
         success: false,
-        message: "User not found"
+        message: "User not found",
       });
-
     }
     req.user = user;
     console.log("req.user set:", req.user);
 
-
     next();
-
-
   } catch (error) {
-
     console.log("Auth Error:", error.message);
 
-
     return res.status(401).json({
-      success:false,
-      message:"Not authorized, token invalid"
+      success: false,
+      message: "Not authorized, token invalid",
     });
-
   }
 };
-
 
 module.exports = protect;
