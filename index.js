d69906c2daa3e6cc45763b668472b692d99dfd18
Manasvi Choudhari <160049511+manasvichoudhari@users.noreply.github.com>
2026-07-23 22:12:46 +0530

    updated

--- index.js
+++ index.js
@@ -24,28 +24,17 @@ const app = express();
 
 
 // Middleware
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "http://localhost:5175",
-  "https://your-vercel-app.vercel.app", // deploy ke baad actual Vercel URL
-];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Postman/mobile apps/no-origin requests allow
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://gayatri-namkeen-frontend.onrender.com",
+    ],
     credentials: true,
   })
 );
+
 
 app.use(express.json());
 
